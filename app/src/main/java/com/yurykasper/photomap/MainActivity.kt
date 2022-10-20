db536afa2a4a0389dd2687d4433a5d92f9af328b
Yury Kasper <kasperyuriu@gmail.com>
2022-10-20 13:06:23 +0300

    Add auth ui and navigation

--- app/src/main/java/com/yurykasper/photomap/MainActivity.kt
+++ app/src/main/java/com/yurykasper/photomap/MainActivity.kt
@@ -2,12 +2,20 @@ package com.yurykasper.photomap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.loginContainerView))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.loginContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
