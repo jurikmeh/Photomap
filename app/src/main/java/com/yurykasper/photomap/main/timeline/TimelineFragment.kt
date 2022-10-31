c72f7c12ad0afb433d238307393a04452681f2e4
Yury Kasper <kasperyuriu@gmail.com>
2022-10-31 16:29:04 +0300

    Create list model and timeline item view

--- app/src/main/java/com/yurykasper/photomap/main/timeline/TimelineFragment.kt
+++ app/src/main/java/com/yurykasper/photomap/main/timeline/TimelineFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yurykasper.photomap.R
+import com.yurykasper.photomap.databinding.FragmentTimelineBinding
 
 class TimelineFragment : Fragment() {
+
+    private lateinit var binding: FragmentTimelineBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false)
+        binding =  FragmentTimelineBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
---
+++ app/src/main/java/com/yurykasper/photomap/models/PhotoDTO.kt
@@ -0,0 +1,14 @@
+package com.yurykasper.photomap.models
+
+import android.location.Location
+import java.util.Date
+
+data class PhotoDTO(
+    val name: String,
+    val description: String,
+    val category: String,
+    val addingDate: Date,
+    val authorId: String,
+    val photoURLs: List<String>,
+    val location: Location
+)
