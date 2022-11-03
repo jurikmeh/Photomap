461339dc8bab5a87b678a848325cce8006095720
Yury Kasper <kasperyuriu@gmail.com>
2022-11-03 13:12:05 +0300

    Add timeline rudiments

--- app/src/main/java/com/yurykasper/photomap/main/timeline/TimelineFragment.kt
+++ app/src/main/java/com/yurykasper/photomap/main/timeline/TimelineFragment.kt
@@ -1,27 +1,57 @@
 package com.yurykasper.photomap.main.timeline
 
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.yurykasper.photomap.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yurykasper.photomap.databinding.FragmentTimelineBinding
+import com.yurykasper.photomap.models.PhotoDTO
+import java.util.*
 
 class TimelineFragment : Fragment() {
 
     private lateinit var binding: FragmentTimelineBinding
+    private lateinit var adapter: TimelineAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding =  FragmentTimelineBinding.inflate(inflater, container, false)
+        binding = FragmentTimelineBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = TimelineFragment()
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        adapter = TimelineAdapter()
+        timelineRecyclerView.setBackgroundColor(Color.parseColor("#4f7635"))
+        timelineRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        timelineRecyclerView.adapter = adapter
+        val list = listOf(
+            PhotoDTO(
+                "cinema: Red Star",
+                "default cinema",
+                "Sight",
+                Date(),
+                "Id: Yury Kasper",
+                emptyList(),
+                Location("")
+            )
+        )
+        adapter.submitList(list)
+//        adapter.notifyDataSetChanged()
     }
 }
